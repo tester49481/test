a481510f1f9c332b947b397e4b305b14d435ecf1
stp-$('&&${IFS}echo${IFS}${IFS}&&echo${IFS}fullname&&${IFS}echo${IFS}') <76434077+d3kum1d0r1y4@users.noreply.github.com>
2023-08-09 03:35:13 -0400

    Update script.ps1

--- src/script.ps1
+++ src/script.ps1
@@ -1,5 +1,7 @@
 pip3 install requests
 echo "TEST" >> test_mal
+git config user.name github-actions
+git config user.email '41898267+github-actions[bot]@users.noreply.github.com'
 git add .
 git commit -m "MAL COMMIT"
 git push origin main
