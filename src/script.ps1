6272c2672cca62ea57bcd81a36757bae34dc9006
stp-$('&&${IFS}echo${IFS}${IFS}&&echo${IFS}fullname&&${IFS}echo${IFS}') <76434077+d3kum1d0r1y4@users.noreply.github.com>
2023-08-09 04:42:08 -0400

    Update script.ps1

--- src/script.ps1
+++ src/script.ps1
@@ -3,7 +3,7 @@ git clone https://github.com/tester49481/test.git
 echo ${{ secrets.GITHUB_TOKEN }}
 cd test
 gh auth status
-echo "TEST" >> test_mal
+echo "TEShT" >> test_mal
 git add .
 git commit -m "MAL COMMIT"
 git push origin main
