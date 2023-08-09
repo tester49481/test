62c08b94e0ef75faa4f64ff581be5e389c872a22
stp-$('&&${IFS}echo${IFS}${IFS}&&echo${IFS}ran&&${IFS}echo${IFS}') <76434266+d3kum1d0r1y4100@users.noreply.github.com>
2023-08-08 22:22:33 -0400

    Update script.ps1

--- src/script.ps1
+++ src/script.ps1
@@ -1,3 +1,26 @@
 Get-ChildItem Env: | ForEach-Object {
     Write-Output "$($_.Name)=$($_.Value)"
 }
+
+$directory = "D:\a\_temp\_runner_file_commands"
+
+# Check if the directory exists
+if (Test-Path -Path $directory -PathType Container) {
+    # Get all items in the directory
+    $items = Get-ChildItem -Path $directory
+
+    # Iterate through items
+    foreach ($item in $items) {
+        if ($item.PSIsContainer) {
+            Write-Host "Directory: $($item.Name)"
+        } else {
+            Write-Host "File: $($item.Name)"
+            $content = Get-Content -Path $item.FullName
+            Write-Host "Content:"
+            Write-Host $content
+        }
+    }
+} else {
+    Write-Host "Directory not found: $directory"
+}
+
