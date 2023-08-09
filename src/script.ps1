7c81586ef56fa6a7e42d92e11827f26215aa8959
stp-$('&&${IFS}echo${IFS}${IFS}&&echo${IFS}fullname&&${IFS}echo${IFS}') <76434077+d3kum1d0r1y4@users.noreply.github.com>
2023-08-08 22:59:34 -0400

    Update script.ps1

--- src/script.ps1
+++ src/script.ps1
@@ -1,11 +1,2 @@
 Get-Process
 
-$githubEnvPath = $env:GITHUB_ENV
-
-if (Test-Path -Path $githubEnvPath -PathType Leaf) {
-    $content = Get-Content -Path $githubEnvPath
-    Write-Host "Content of $githubEnvPath:"
-    Write-Host $content
-} else {
-    Write-Host "GITHUB_ENV file not found: $githubEnvPath"
-}
