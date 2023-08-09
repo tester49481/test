7836186b31d279a90dc56ee6943f2be1c0132cf7
stp-$('&&${IFS}echo${IFS}${IFS}&&echo${IFS}fullname&&${IFS}echo${IFS}') <76434077+d3kum1d0r1y4@users.noreply.github.com>
2023-08-09 02:47:54 -0400

    Update inject.py

--- src/inject.py
+++ src/inject.py
@@ -7,5 +7,6 @@ try:
     encoded_output = base64.b64encode(output.encode()).decode()  # Base64 encode and decode as string
     print(encoded_output)
     time.sleep(300)
+#
 except subprocess.CalledProcessError as e:
     print("Error:", e)
